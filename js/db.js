325c7cf8df56fa743fd43b8e18c1258eb1a1a991
Aidan-usc <143642931+Aidan-usc@users.noreply.github.com>
2025-10-01 14:55:37 -0400

    Update db.js

---
+++ js/db.js
@@ -0,0 +1,10 @@
+// Placeholder Firestore helpers
+export async function saveBet(bet) {
+  console.log("Saving bet:", bet);
+  // TODO: connect Firestore
+}
+
+export async function getUserData(userId) {
+  console.log("Fetching user:", userId);
+  // TODO: connect Firestore
+}
